bb6a3611e58b8968ecc9b72c00539ff5626460c8
Shanshan Wu <790219068@qq.com>
2025-04-10 01:13:05 -0700

    feat: enhance styling with custom colors and card-feature class

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'baby-blue': '#89CFF0',
+        'light-blue': '#BFE6FF',
+        'vibrant-green': '#7FFFD4',
+        'light-green': '#AAFCE9',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
